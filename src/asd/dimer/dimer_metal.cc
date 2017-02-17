3b5180b552ac96b88f829a6d1af2164041e17d41
Raymond Wang <raymondwang@u.northwestern.edu>
2017-02-17 14:37:46 -0600

    minor

--- src/asd/dimer/dimer_metal.cc
+++ src/asd/dimer/dimer_metal.cc
@@ -154,10 +154,6 @@ void Dimer::set_active_metal(shared_ptr<const PTree> idata) {
       cout << "(" << i << ") = " << *compare->element_ptr(i,i) << endl;
 #endif
       
-      auto tempref = make_shared<Reference>(sgeom_, make_shared<const Coeff>(move(*coeff)), isolated_refs_.first->nclosed(), isolated_refs_.first->nact(),
-                                             isolated_refs_.first->nvirt());
-      isolated_refs_ = {tempref, tempref};
-
       for (auto& amo : Llist) {
         const double sum_A = blas::dot_product(coeff->element_ptr(bounds[0].first, amo), bounds[0].second - bounds[0].first,coeff->element_ptr(bounds[0].first, amo));
         const double sum_B = blas::dot_product(coeff->element_ptr(bounds[1].first, amo), bounds[1].second - bounds[1].first,coeff->element_ptr(bounds[1].first, amo));
@@ -176,11 +172,39 @@ void Dimer::set_active_metal(shared_ptr<const PTree> idata) {
           throw runtime_error("Wrong choice of active orbitals. The projected orbital(" + to_string(amo+1) + ") does not belong to any monomer.");
       }
     } // end of !Llist.empty() 
-  } // Now we have new orthonormal coeff as well as active orbitals.
+  } // Now we have new orthonormal coeff as well as active orbital lists.
 
   // Make new Reference; active orbitals are placed after closed orbitals
-  active_refs_ = {isolated_refs_.first->set_active(Alist), isolated_refs_.second->set_active(Blist)};
+  auto tmpref = isolated_refs_.first->set_active_metal(Alist, Blist);
+  active_refs_ = {tmpref, tmpref};
 
+  // Update dimer info
+  const int nclosedA = active_refs_.first->nclosed();
+  const int nclosedB = active_refs_.second->nclosed();
+  const int nactA = active_refs_.first->nact();
+  const int nactB = active_refs_.second->nact();
+  const int nact = nactA + nactB;
+  const int nactvirtA = isolated_refs_.first->nvirt() - active_refs_.first->nvirt();
+  const int nactvirtB = isolated_refs_.second->nvirt() - active_refs_.second->nvirt();
+  const int dimerbasis = sgeom_->nbasis();
+  assert(dimerbasis == geoms_.first->nbasis());
+  assert(dimerbasis == geoms_.second->nbasis());
+  const int nclosed_HF = sref_->nclosed();
+  const int nvirt_HF = sref_->nvirt();
+  assert(dimerbasis == nclosed_HF + nvirt_HF);
+  assert(sref_->nact() == 0);
+  const int nclosed = nclosed_HF - (nclosed_HF - nclosedA) - (nclosed_HF - nclosedB);
+cout << "nclosedA = " << nclosedA << endl;
+cout << "nclosedB = " << nclosedB << endl;
+cout << "nactA = " << nactA << endl;
+cout << "nactB = " << nactB << endl;
+cout << "nact = " << nact << endl;
+cout << "nactvirtA = " << nactvirtA << endl;
+cout << "nactvirtB = " << nactvirtB << endl;
+cout << "dimerbasis = " << dimerbasis << endl;
+cout << "nclosed_HF = " << nclosed_HF << endl;
+cout << "nvirt_HF = " << nvirt_HF << endl;
+cout << "nclosed = " << nclosed << endl;
 
 
 }
